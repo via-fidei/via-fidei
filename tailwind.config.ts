ebff3efb23cd60a9b4b17d062ef176c08bbe2df6
Matheus Vieira <matheusmczvieira@gmail.com>
2025-04-19 11:21:55 -0300

    chore: initial shadcn setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,50 +9,98 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      colors: {
-        "light-primary": "#6E59A5",
-        "light-secondary": "#F1F0FB",
-        "light-accent": "#8B5CF6",
-        "light-hover": "#7E69AB",
-        "light-text-primary": "#1A1F2C",
-        "light-text-secondary": "#64748B",
-        "light-border": "#E2E8F0",
-        "dark-primary": "#6E59A5",
-        "dark-secondary": "#1A1F2C",
-        "dark-accent": "#9B87F5",
-        "dark-hover": "#D6BCFA",
-        "dark-text-primary": "#F8FAFC",
-        "dark-text-secondary": "#94A3B8",
-        "dark-border": "#334155",
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
+  	container: {
+  		center: true,
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
+  	extend: {
+  		colors: {
+  			'light-primary': '#6E59A5',
+  			'light-secondary': '#F1F0FB',
+  			'light-accent': '#8B5CF6',
+  			'light-hover': '#7E69AB',
+  			'light-text-primary': '#1A1F2C',
+  			'light-text-secondary': '#64748B',
+  			'light-border': '#E2E8F0',
+  			'dark-primary': '#6E59A5',
+  			'dark-secondary': '#1A1F2C',
+  			'dark-accent': '#9B87F5',
+  			'dark-hover': '#D6BCFA',
+  			'dark-text-primary': '#F8FAFC',
+  			'dark-text-secondary': '#94A3B8',
+  			'dark-border': '#334155',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: 0
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: 0
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
