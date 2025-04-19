a8ac30e74139363b3fc9e780c1f71879002062a4
Matheus Vieira <matheusmczvieira@gmail.com>
2025-04-19 12:30:06 -0300

    chore: Theme provider and theme store

---
+++ src/store/theme.ts
@@ -0,0 +1,24 @@
+"use client";
+
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface ThemeStore {
+  theme: "light" | "dark";
+  toggleTheme: () => void;
+  setTheme: (theme: "light" | "dark") => void;
+}
+
+export const useThemeStore = create<ThemeStore>()(
+  persist(
+    (set, get) => ({
+      theme: "light",
+      toggleTheme: () =>
+        set({ theme: get().theme === "light" ? "dark" : "light" }),
+      setTheme: (theme) => set({ theme }),
+    }),
+    {
+      name: "viafidei-theme",
+    }
+  )
+);
